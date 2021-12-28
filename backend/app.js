042eb00ba1f6050e84d94accd89e0f2d98e4c3be
Balu Babu <balub997@gmail.com>
2021-12-28 23:35:02 +0530

    made 2 queries to fetch all pokemon and individal pokemon

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { buildSchema } from "graphql";
+
+import { schema } from "./schema.js";
 
 var app = express();
 
 app.use(
-    "/graphql",
-    graphqlHTTP({
-        graphiql: true,
-    })
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
 );
 
-app.listen(3000, () => {
-    console.log("server running...");
+app.listen(4000, () => {
+  console.log("server running...");
 });
---
+++ backend/schema.js
@@ -0,0 +1,52 @@
+import {
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+import axios from "axios";
+
+const PokemonType = new GraphQLObjectType({
+  name: "PokemonType",
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    sprite: {
+      type: GraphQLString,
+      resolve(parentValue) {
+        return parentValue.pokemon_data.sprites.other.dream_world.front_default;
+      },
+    },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  type: "RootQuery",
+  name: "Pokemon",
+  fields: {
+    pokemon: {
+      type: PokemonType,
+      args: { id: { type: GraphQLInt } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/pokemon/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+    all_pokemon: {
+      type: new GraphQLList(PokemonType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/pokemon`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+export { schema };
