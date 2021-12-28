872a09ec4697515348953991177ab247901a1b16
Balu Babu <balub997@gmail.com>
2021-12-29 01:23:15 +0530

    Addec CORS to express backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-
+import { cors } from "cors";
 import { schema } from "./schema/schema.js";
 
 var app = express();
+
+app.use(cors());
 
 app.use(
   "/graphql",
