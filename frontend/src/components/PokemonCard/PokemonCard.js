b9ecb19e5c31b6fbabff7965d20620c56602d636
Balu Babu <balub997@gmail.com>
2021-12-29 01:42:58 +0530

    able to display all pokemon data

--- frontend/src/components/PokemonCard/PokemonCard.js
+++ frontend/src/components/PokemonCard/PokemonCard.js
@@ -3,20 +3,18 @@ import PokemonTypeChip from "../PokemonTypeChip/PokemonTypeChip";
 
 import "./styles.scss";
 
-function PokemonCard() {
+function PokemonCard({ data }) {
+  const { sprite, name, pokemon_type } = data;
   return (
     <div className="Card">
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-        alt="pokemon"
-      />
+      <img src={sprite} alt="pokemon" />
       <div className="Card-Content">
         <div className="Card-Chips">
           <PokemonTypeChip />
           <PokemonTypeChip />
           <PokemonTypeChip />
         </div>
-        <p>Pokemon Name</p>
+        <p>{name.toUpperCase()}</p>
       </div>
     </div>
   );
